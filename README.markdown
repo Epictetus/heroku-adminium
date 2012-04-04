a45383e03b19c3d6c04fff3b29a7c2ff3d71449f
Jessy Bernal <jessy.benal@gmail.com>
2012-04-04 14:11:30 +0200

    look also for CLEARDB_DATABASE_URL config var

--- README.markdown
+++ README.markdown
@@ -1,1 +1,4 @@
 Heroku plugin to enable the usage of the [Adminium Heroku addon](http://addons.heroku.com/adminium).
+
+In order to provide you with an administration interface for your data, this plugin will look for your DATABASE_URL Heroku config var and store an encrypted version. Removing the addon will delete that info from the Adminium database.
+
--- init.rb
+++ init.rb
@@ -3,14 +3,14 @@ module Heroku::Command
     Help.group("Adminium") do |group|
       group.command "adminium", "enable adminium for the current app"
     end
-    
+
     def initialize(*args)
       super
       @config_vars = heroku.config_vars(app)
       @adminium_url = @config_vars["ADMINIUM_URL"]
       abort " !   Please add the adminium addon first." unless @adminium_url
     end
-    
+
     def index
       puts "In order to provide you with an administration interface for your data,"
       puts "Adminium will store an encrypted version of the DATABASE_URL of your application"
@@ -18,13 +18,38 @@ module Heroku::Command
       puts "Do you confirm the plugin execution ? (yes / no)"
       print "> "
       answer = STDIN.gets
-      if answer.chomp == 'yes'
-        RestClient::Resource.new(@adminium_url).put :account => {:db_url => @config_vars['DATABASE_URL']}
-        puts "Successfully enabled Adminium for the current app."
+      if ['yes', 'y'].include? answer.chomp.downcase
+        if db_urls.empty?
+          puts "We did not find any DATABASE_URL for your application, please provide us manually on our addon web page"
+        else
+          if db_urls.length > 1
+            puts "We found several database urls you may want to connect to, please choose one :"
+            db_urls.each_with_index { |db, index| puts "#{index + 1}. #{db[:key]} => #{db[:value]}" }
+            begin
+              answer = STDIN.gets
+            end while answer.chomp.to_i <= 0 || answer.chomp.to_i > db_urls.length
+            db_url_index = answer.chomp.to_i - 1
+          else
+            db_url_index = 0
+          end
+          RestClient::Resource.new(@adminium_url).put :account => {:db_url => db_urls[db_url_index][:value]}
+          puts "Successfully enabled Adminium for the current app."
+        end
       else
         puts "Plugin execution aborted."
       end
     end
 
+    def db_urls
+      return @db_urls unless @db_urls.nil?
+      @db_urls = []
+      ['DATABASE_URL', 'CLEARDB_DATABASE_URL', 'CLEARDB_DATABASE_URL_A', 'CLEARDB_DATABASE_URL_B', 'SHARED_DATABASE_URL'].each do |key|
+        if @config_vars.has_key?(key) && !@db_urls.map{|d| d[:value]}.include?( @config_vars[key])
+          @db_urls << {:key => key, :value => @config_vars[key]}
+        end
+        @db_urls
+      end
+
+    end
   end
 end
